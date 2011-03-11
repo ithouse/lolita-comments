b94a5deb8c9d3fa4bd25ece04e40b02bf8ba1ae6
Arturs Meisters <arturs@ithouse.lv>
2011-03-11 15:39:53 +0200

    install generator fixed; now ask if need to create new migration

--- lib/generators/lolita_comments/install_generator.rb
+++ lib/generators/lolita_comments/install_generator.rb
@@ -1,16 +1,25 @@
-require 'rails/generators/active_record'
 module LolitaComments
   module Generators
-    class InstallGenerator < ActiveRecord::Generators::Base
-
-      @@root=File.expand_path("#{__FILE__}/../../../..")
-      
-      hook_for :orm
+    class InstallGenerator < Rails::Generators::Base
+      source_root File.join(File.dirname(__FILE__),"templates")
       
       def copy_migrations
-        migration_template "migration.rb"
+        if defined?(ActiveRecord)
+          filename="#{Time.now.strftime("%Y%m%d%H%M%S")}_lolita_create_comments.rb"
+          exist=Dir.new(File.join(Rails.root,"db","migrate")).detect{|f| 
+            f.match(/\d{14}_lolita_create_comments\.rb/)
+          }
+          if exist
+            print "Migration already exit #{filename}! Create new? [Yn] "
+            do_copy=gets
+            do_copy=do_copy=="Y" || do_copy=="y" ? true : false
+          else 
+            do_copy=true
+          end
+          template "migration.rb", "db/migrate/#{filename}" if do_copy
+        end
       end
-      
+
     end
   end
 end
--- lib/lolita-comments.rb
+++ lib/lolita-comments.rb
@@ -1,1 +1,2 @@
 require "#{File.dirname(__FILE__)}/lolita-comments/rails"
+require "#{File.dirname(__FILE__)}/lolita-comments/version"
